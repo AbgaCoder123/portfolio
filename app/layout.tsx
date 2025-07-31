85e2b761224d7050b7ba533cc86cf10eda2c87aa
Code with Ben <bernardgodsgift@gmail.com>
2025-07-31 17:27:55 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      ></link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap"
+        rel="stylesheet"
+      ></link>
       <body className={inter.className}>{children}</body>
     </html>
   );
