9f8dd97a1e2133fc55319bcb4851147a74af7043
Code with Ben <bernardgodsgift@gmail.com>
2025-07-31 17:34:00 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -610,7 +610,7 @@ export default function Portfolio() {
       {/* Hero Section */}
 
       <div
-        className={`min-h-screen w-full max-sm:px-5 max-sm:py-10 ${
+        className={`min-h-screen w-full max-sm:px-5 max-[400px]:pt-[72px] max-[400px]:pb-[72px] ${
           isDarkMode ? "bg-[#1f222a]" : "bg-[#f6f8fb]"
         } ${textColor} `}
       >
